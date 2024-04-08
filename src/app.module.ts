33cccaf222b9c2c9b1a2755da4de692b884d50e3
Miroslav Shkretov <miro_shkretov@abv.bg>
2024-04-09 00:31:27 +0300

    add env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
@@ -10,18 +11,18 @@ import { UserOrdersModule } from './user-orders/user-orders.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'csmanqka2',
-      database: 'nestapi',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'false',
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/restfullapi', {
-    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
     AuthModule,
     ProductsModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,30 @@
+// auth.module.ts
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'test' || 'miro',
-      signOptions: { expiresIn: '3600s' },
+    ConfigModule,
+    JwtModule.registerAsync({ 
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3600s' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
-  controllers: [AuthController], // Add AuthController here
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
